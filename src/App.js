49e9a0c53552ca7fb49e0775662c940c443b859f
Rajat <rajat@flyvoly.com>
2021-10-09 14:48:17 -0700

    Added CSS for styling the calculator

--- src/App.js
+++ src/App.js
@@ -4,35 +4,35 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="input"></div>
-        <div className="row first-row">
+        <div className="input">0</div>
+        <div className="row">
           <button className="btn btn-grey">AC</button>
           <button className="btn btn-grey" disabled={true}>%</button>
           <button className="btn btn-grey" disabled={true}>+/-</button>
           <button className="btn btn-orange">÷</button>
         </div>
-        <div className="row second-row">
+        <div className="row">
           <button className="btn btn-grey">7</button>
           <button className="btn btn-grey">8</button>
           <button className="btn btn-grey">9</button>
           <button className="btn btn-orange">X</button>
         </div>
-        <div className="row third-row">
+        <div className="row">
           <button className="btn btn-grey">4</button>
           <button className="btn btn-grey">5</button>
           <button className="btn btn-grey">6</button>
           <button className="btn btn-orange">-</button>
         </div>
-        <div className="row fourth-row">
+        <div className="row">
           <button className="btn btn-grey">1</button>
           <button className="btn btn-grey">2</button>
           <button className="btn btn-grey">3</button>
           <button className="btn btn-orange">+</button>
         </div>
-        <div className="row fifth-row">
-          <button className="btn btn-grey">0</button>
-          <button className="btn btn-grey">.</button>
-          <button className="btn btn-grey">=</button>
+        <div className="row">
+          <button className="btn btn-grey btn-0">0</button>
+          <button className="btn btn-grey" disabled={true}>.</button>
+          <button className="btn btn-orange">=</button>
         </div>
       </div>
     </div>
