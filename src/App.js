0d01521324e094c9452d6712cfd40e09eadb201d
Rajat <rajat@flyvoly.com>
2021-10-09 13:48:58 -0700

    Basic UI with keys no CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Placeholder for Mac OS X
+      <div className="container">
+        <div className="input"></div>
+        <div className="row first-row">
+          <button className="btn btn-grey">AC</button>
+          <button className="btn btn-grey" disabled={true}>%</button>
+          <button className="btn btn-grey" disabled={true}>+/-</button>
+          <button className="btn btn-orange">÷</button>
+        </div>
+        <div className="row second-row">
+          <button className="btn btn-grey">7</button>
+          <button className="btn btn-grey">8</button>
+          <button className="btn btn-grey">9</button>
+          <button className="btn btn-orange">X</button>
+        </div>
+        <div className="row third-row">
+          <button className="btn btn-grey">4</button>
+          <button className="btn btn-grey">5</button>
+          <button className="btn btn-grey">6</button>
+          <button className="btn btn-orange">-</button>
+        </div>
+        <div className="row fourth-row">
+          <button className="btn btn-grey">1</button>
+          <button className="btn btn-grey">2</button>
+          <button className="btn btn-grey">3</button>
+          <button className="btn btn-orange">+</button>
+        </div>
+        <div className="row fifth-row">
+          <button className="btn btn-grey">0</button>
+          <button className="btn btn-grey">.</button>
+          <button className="btn btn-grey">=</button>
+        </div>
+      </div>
     </div>
   );
 }
